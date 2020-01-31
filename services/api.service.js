651ef5be7af3ca4783540b801d53c68a3d198063
VictorNicolasLO <victornicolaslo@hotmail.com>
2020-01-31 11:39:12 -0700

    init

---
+++ services/api.service.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const ApiGateway = require("moleculer-web");
+
+module.exports = {
+	name: "api",
+	mixins: [ApiGateway],
+
+	// More info about settings: https://moleculer.services/docs/0.13/moleculer-web.html
+	settings: {
+		port: 3000,
+		routes: [
+			{
+				path: "/api/projects",
+				aliases: {
+					"POST create-project": "rpc.project-management.createProject",
+				},
+				whitelist: [
+					// Access to any actions in all services under "/api" URL
+					"**",
+				],
+			},
+		],
+
+		// Serve assets from "public" folder
+		assets: {
+			folder: "public",
+		},
+	},
+};
